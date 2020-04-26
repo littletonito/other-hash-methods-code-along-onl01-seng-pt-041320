38d2826aea69f704058ff25e9329ed4e33deb8b7
Antonio Clavijo Cortes <littletonito@gmail.com>
2020-04-26 18:34:10 +0000

    Automatically backed up by Learn

--- lib/values.rb
+++ lib/values.rb
@@ -8,6 +8,9 @@ require 'pry'
 #  grains: ["rice", "pasta"]
 # }
 
-def get_the_values(groceries)
-  #code your solution here!
-end
+# def get_the_values(groceries)
+#   #code your solution here!
+# end
+
+
+
